0e263daed64d0a75032bff1093d5ca02cd0261fe
Tachibana Shin <tachibshin@duck.com>
2023-08-11 01:40:37 +0000

    fix test

--- test/vitest/setup-file.ts
+++ test/vitest/setup-file.ts
@@ -1,2 +1,11 @@
 // This file will be run before each test file
 import "fake-indexeddb/auto"
+
+const { Date } = self
+;(self as unknown as never).Date = class extends Date {
+  constructor(value: string | number | Date) {
+    if (typeof value === "string") value += " GMT+0"
+
+    super(value)
+  }
+}
