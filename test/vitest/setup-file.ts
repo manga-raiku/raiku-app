906c00bb9a3fa0343deb7f0fc3520b6f74c57eac
Tachibana Shin <tachibshin@duck.com>
2023-08-11 02:56:08 +0000

    rm log

--- test/vitest/setup-file.ts
+++ test/vitest/setup-file.ts
@@ -5,12 +5,9 @@ const { Date } = self
 Object.assign(self, {
   Date: class extends Date {
     constructor(value: string | number | Date) {
-      const raw = value
       if (typeof value === "string" && !value.includes("+")) value += " GMT+0"
 
       super(value)
-
-      if (Number.isNaN(this.getTime())) console.error(raw)
     }
   },
 })
