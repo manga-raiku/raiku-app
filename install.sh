ad994eaf1476d9393814827736562a17ca09cfc2
Tachibana Shin <tachibshin@duck.com>
2023-09-07 17:34:29 +0700

    fix hang test if not exists .env

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# blend S
+if [ ! -f .env ]; then
+  cp .env.example .env
+  echo "Copied .env.example => .env"
+else
+  echo "File .env exists"
+fi
 
 sudo apt update -y
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
