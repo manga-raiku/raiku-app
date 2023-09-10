a70522644cb0a21b0b5c0faa8e6bb6797d5b40fb
Tachibana Shin <tachibshin@duck.com>
2023-09-10 08:55:09 +0000

    fix scripts release:pwa

--- scripts/bumpp-pwa.ts
+++
@@ -1,6 +0,0 @@
-import { spawnSync } from "child_process"
-
-import { bold, green } from "kleur"
-
-console.log(bold(green(" === Bumpp version for PWA === ")))
-spawnSync("bumpp", ["--commit", "(chore): release PWA %s", "--tag", "pwa@%s"])
