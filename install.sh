b1fc0ea5f646d6f64ab76b94dd2685afb50ed8c3
Tachibana Shin <tachibshin@duck.com>
2023-09-29 16:31:54 +0000

    add pre build raiku-pgs

--- install.sh
+++ install.sh
@@ -10,5 +10,9 @@ fi
 
 sudo apt update -y
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
-pnpm i
+pnpm i -r
+cd npm/raiku-pgs
+pnpm build
+pnpm i -r
+cd ../..
 npx cypress install
