b0e3eae26a7b246064e9d9c407a677ad0f1dbfad
Tachibana Shin <tachibshin@duck.com>
2023-10-10 10:41:15 +0000

    fix loop wait

--- src/logic/with-cache.ts
+++ src/logic/with-cache.ts
@@ -25,7 +25,6 @@ export async function withCache<T extends object>(
         }
         void set(uniKey, JSON.stringify(data))
       })
-      .catch((err) => console.error(err))
   ])
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
