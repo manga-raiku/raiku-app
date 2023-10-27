7c6cc12335fc8a48cc8a9f4c334d87eb5e2eb12d
Tachibana Shin <tachibshin@duck.com>
2023-10-27 10:31:54 +0700

    add nativescript files

---
+++ src/nativescript/index.ts
@@ -0,0 +1,16 @@
+// init - keep here.
+import "@nativescript/capacitor/bridge"
+
+/**
+ *      ****       ****
+ *      ******     ****
+ *      ********   ****
+ *    ****** ***** ******  NativeScript
+ *      ****   ********
+ *      ****     ******
+ *      ****       ****
+ *
+ *    🧠  Learn more:  👉  https://capacitor.nativescript.org/getting-started.html
+ */
+
+native.sum = (a, b) => a + b
