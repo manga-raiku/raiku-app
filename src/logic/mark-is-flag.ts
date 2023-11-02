24c6dae3614f877f6018da2e2feb976b27621dcc
Tachibana Shin <tachibshin@duck.com>
2023-11-02 05:11:15 +0000

    chore: flre io

--- src/logic/mark-is-flag.ts
+++ src/logic/mark-is-flag.ts
@@ -3,6 +3,11 @@ export function markFlag<T extends object>(data: T, name: string): T {
   return data
 }
 
+export function unFlag<T extends object>(data: T, name: string): T{
+  delete data[name as keyof T]
+  return data
+}
+
 export function isFlag<T extends object>(data: T, name: string): boolean {
   return name in data
 }
