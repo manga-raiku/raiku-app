70c67dae2445958ff7a2d87bf234ba41d3ca05c1
Tachibana Shin <tachibshin@duck.com>
2023-11-09 11:16:29 +0000

    chore: [raiku-pgs]: upgradeToHttps

--- npm/raiku-pgs/lib/plugin/logic/upgrade-to-https.ts
+++ npm/raiku-pgs/lib/plugin/logic/upgrade-to-https.ts
@@ -2,5 +2,7 @@ export function upgradeToHttps(url: string) {
   if (url.startsWith("http://")) {
     return `https${url.slice(4)}`
   }
+  if (url.startsWith("://")) return `https${url}`
+
   return url
 }
