f5119b53c3c17c2113663549194b63199ff7708f
Tachibana Shin <tachibshin@duck.com>
2023-11-14 09:20:35 +0000

    chore: fix esm

--- npm/raiku-pgs/.eslintrc.js
+++ npm/raiku-pgs/.eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   extends: require.resolve("../../.eslintrc.js"),
   parserOptions: {
     parser: "@typescript-eslint/parser",
---
+++ npm/raiku-pgs/scripts/package-module.ts
@@ -0,0 +1,9 @@
+import { readFileSync, writeFileSync } from "fs"
+import { join } from "path"
+
+const pkg = JSON.parse(
+  readFileSync(join(__dirname, "..", "package.json"), "utf8")
+)
+pkg.type = "module"
+
+writeFileSync(join(__dirname, "..", "package.json"), JSON.stringify(pkg, null , 2))
