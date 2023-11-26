940ab53440d83149e52bb8ccd0b4f745ceba4d82
Tachibana Shin <tachibshin@duck.com>
2023-11-26 12:08:33 +0000

    chore: fix request permission notify

--- src/boot/local-notification.ts
+++ src/boot/local-notification.ts
@@ -2,6 +2,8 @@ import { LocalNotifications } from "@capacitor/local-notifications"
 
 export const permissionNotification: Promise<void> = (async () => {
   if ((await LocalNotifications.checkPermissions()).display !== "granted") {
-    await LocalNotifications.requestPermissions()
+    const requestPermissions = async () =>
+      await LocalNotifications.requestPermissions()
+    window.addEventListener("click", requestPermissions, { once: true })
   }
 })()
